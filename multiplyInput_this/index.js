a8fc0ee1de4616a431c8ac6a2bb8747e6bd3fa48
lavpalve <53850659+lavpalve@users.noreply.github.com>
2021-12-13 13:44:21 +0530

    Update index.js

--- multiplyInput_this/index.js
+++ multiplyInput_this/index.js
@@ -7,7 +7,7 @@ multiplyBtn.addEventListener('click', function(){
     var input2 = document.getElementById('input2').value;
     var input3 = document.getElementById('input3').value;
 
-    console.log(input1);
+    //console.log(input1);
 
     multiplyObj = {
         input_1: input1,
